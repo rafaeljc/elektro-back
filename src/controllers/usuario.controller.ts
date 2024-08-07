51caad06332bf66c81775d1d3c79a73d0787c387
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2024-08-07 14:24:38 -0300

    Adicionado método update

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -4,6 +4,14 @@ import geraHashSalt from "../config/auth";
 
 const prisma = new PrismaClient();
 
+type InputUsuario = {
+  nome?: string,
+  email?: string,
+  dataNascimento?: Date,
+  hash?: string,
+  salt?: string,
+}
+
 class UsuarioController {
   public async create(request: Request, response: Response) {
     try {
@@ -74,6 +82,42 @@ class UsuarioController {
     } catch (error) {
       return response.status(500).json({
         mensagem: "Não foi possível buscar todos usuários."
+      });
+    }
+  }
+
+  public async update(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+      const { nome, email, dataNascimento, senha } = request.body;
+      
+      // definindo informações que serão atualizadas
+      let input: InputUsuario = {};
+      if (nome) input.nome = nome;
+      if (email) input.email = email;
+      if (dataNascimento) input.dataNascimento = new Date(dataNascimento);
+      if (senha) {
+        const { hash, salt } = geraHashSalt(senha);
+        input.hash = hash;
+        input.salt = salt;
+      }
+
+      const usuarioAtualizado = await prisma.usuario.update({
+        where: {
+          id: Number(id)
+        },
+        data: input,
+        select: {
+          nome: true,
+          email: true,
+          dataNascimento: true,
+        }
+      });
+
+      return response.status(200).json(usuarioAtualizado);
+    } catch (error) {
+      return response.status(500).json({
+        mensagem: "Não foi possível atualizar os dados do usuário."
       });
     }
   }
